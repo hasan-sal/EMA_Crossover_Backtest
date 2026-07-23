48f4e0028ba807a8988ae3628ddb69516bfe853b
Hasan Salihoglu <salihogl@purdue.edu>
2026-07-23 12:55:13 +0300

    Update report.py

--- report.py
+++ report.py
@@ -1,12 +1,11 @@
 import matplotlib.pyplot as plt
 import pandas as pd 
-import numpy as np
 import backtest
 
 
 
 position = False
-total_gain, compound_annual_growth_rate, total_trades, portfolio_values = backtest.backtest_strategy("BTC-USD", "2017-01-01", "2026-01-01", 7, 100, 20)  
+total_gain, compound_annual_growth_rate, total_trades, portfolio_values = backtest.backtest_strategy("BTC-USD", "2026-01-01", "2026-07-12x", 7, 100, 20)  
 
 plt.plot(pd.DataFrame(portfolio_values)["date"], pd.DataFrame(portfolio_values)["value"])
 plt.title("Portfolio Value Over Time")
